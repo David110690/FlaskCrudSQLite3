2b3d9f9dcfed5d8868034b7ba6b6bb5cd00c3720
David Travezano <Davidu.0622@gmail.com>
2020-04-29 08:19:29 -0500

    Primer commit

---
+++ static/JS/main.js
@@ -0,0 +1,13 @@
+const btnDelete = document.querySelectorAll('.btn-delete') 
+
+if(btnDelete){  
+    const btnArray = Array.from(btnDelete); 
+    btnDelete.forEach((btn) => { 
+        btn.addEventListener('click', (e) => { 
+            if (!confirm('Desea eliminar?')){ 
+                e.preventDefault();
+            }
+        });
+    });
+}
+
